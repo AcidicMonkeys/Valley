2fdde0a5e9b45a668dc3fa4314ab30635c37335d
Hiro-Beet <69685986+Hiro-Beet@users.noreply.github.com>
2022-02-17 20:36:06 +0000

    Update! [⚙️]

--- arm64/nodejs/entrypoint.sh
+++ arm64/nodejs/entrypoint.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-#
-# Copyright (c) 2021 HiroBeet
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+
+#///////////////////////////////////////////////////////////////////////////////////#
+#                                                                                   #
+#                           Copyright (c) 2021 HiroBeet                             #
+#                                                                                   #
+#  Permission is hereby granted, free of charge, to any person obtaining a copy     #
+#  of this software and associated documentation files (the "Software"), to deal    #
+#  in the Software without restriction, including without limitation the rights     #
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        #
+#  copies of the Software, and to permit persons to whom the Software is            #
+#  furnished to do so, subject to the following conditions:                         #
+# --------------------------------------------------------------------------------- #
+#  The above copyright notice and this permission notice shall be included in all   #
+#  copies or substantial portions of the Software.                                  #
+# --------------------------------------------------------------------------------- #
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       #
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         #
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      #
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           #
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    #
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+#  SOFTWARE.                                                                        #
+#                                                                                   #
+#///////////////////////////////////////////////////////////////////////////////////#
 
 #|| Start [📍] ||#
 
-cd /home/container # || Change Directory
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'` # || Make local Docker IP address available for the processes.
+cd /home/container # || Change container's work directory.
+
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'` # || Set a local variable that makes the private Docker IP address available for the processes.
+
 node -v # || Show the actual NodeJs version.
+
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g') # || Replace the Startup command.
+
+echo ":/home/container$ ${MODIFIED_STARTUP}" # || Show the new Startup command.
+
+eval ${MODIFIED_STARTUP} # || Run the server.
